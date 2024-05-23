b871201c2f4217cbdc031b5e864f8973c797e525
KimyFuchs <Karim.u506203@gmail.com>
2024-05-23 09:59:47 -0700

    Miausers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SkillsResolver } from './skills/skills.resolver';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot(process.env.FOX_Url)],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, SkillsResolver],
 })
 export class AppModule {}
